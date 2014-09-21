2c804c13950aefd81ead3a509e49521e9a76eebf
mtnbikerbrad <mtnbikerbrad@gmail.com>
2014-09-21 16:21:50 -0700

    Name Change

---
+++ iOS8_Swift.playground/section-1.swift
@@ -0,0 +1,65 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var firstInt:Int = 0
+var secondInt:Int = 29
+var thirdInt:Float = -120495983
+
+var x:Int = 5
+
+var discountOnSale:Float = 0.3
+
+
+
+var mySpeed = 45
+
+if mySpeed < 70
+{
+    println("Speed up Grammy")
+}
+else
+{
+    println("Now your moving")
+}
+
+let isTelevisionOn = true
+
+if isTelevisionOn == true
+{
+    println("Stop wasting your time and get back to coding")
+}
+else
+{
+    println("Don't get distracted by the TV, it will suck you in with never ending series that go nowhere")
+}
+
+// For Loops
+
+var mediationHours = 1
+
+for mediationHours; mediationHours < 10; mediationHours++
+{
+    println("I am more enlightened \(mediationHours)")
+}
+
+var wheat:Double = 1
+
+for var i = 0; i < 64; i++
+{
+    wheat = wheat * 2
+    println("location on board \(i) and peices of wheat: \(wheat)")
+}
+
+
+for var soda = 99; soda >= 0; soda--
+{
+    if soda == 0
+    {
+        println("You drank them all")
+    }
+    else
+    {
+        println("\(soda) bottlees of soda on the wall")
+    }
+}
