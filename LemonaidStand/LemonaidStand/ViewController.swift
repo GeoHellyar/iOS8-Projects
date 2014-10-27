7f12ac72ded709c91048759bac79bf85472b48f5
mtnbikerbrad <mtnbikerbrad@gmail.com>
2014-10-27 06:38:18 -0700

    Button and Labels named and hookedup

--- LemonaidStand/LemonaidStand/ViewController.swift
+++ LemonaidStand/LemonaidStand/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOfLemonsPuchasedLabel: UILabel!
+    @IBOutlet weak var numberOfIcePurchesedLabel: UILabel!
+    @IBOutlet weak var suppliesCashLabel: UILabel!
+    @IBOutlet weak var suppliesLemonsLabel: UILabel!
+    @IBOutlet weak var suppliesIceLabel: UILabel!
+    @IBOutlet weak var lemonsInMixLable: UILabel!
+    @IBOutlet weak var iceInMixLabel: UILabel!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buyLemonButtonPressed(sender: AnyObject) {
+    }
+    @IBAction func sellLemonButtonPressed(sender: AnyObject) {
+    }
+    @IBAction func buyIceButtonPressed(sender: AnyObject) {
+    }
+    @IBAction func sellIceButtonPressed(sender: AnyObject) {
+    }
 
+    @IBAction func moreLemonsInMixButtonPressed(sender: AnyObject) {
+    }
+    @IBAction func lessLemonsInMixButtonPressed(sender: AnyObject) {
+    }
+    @IBAction func moreIceInMixButtonPressed(sender: AnyObject) {
+    }
+    @IBAction func lessIceInMixButtonPressed(sender: AnyObject) {
+    }
+
+    @IBAction func sellLomonaidButtonPressed(sender: AnyObject) {
+    }
 }
 
