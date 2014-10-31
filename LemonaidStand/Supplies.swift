445ce69c220515ec4cba096624dc9504ad3a9746
mtnbikerbrad <mtnbikerbrad@gmail.com>
2014-10-31 08:19:33 -0700

    Supplies Struct Added

---
+++ LemonaidStand/Supplies.swift
@@ -0,0 +1,14 @@
+//
+//  Supplies.swift
+//  LemonaidStand
+//
+//  Created by Brad Hellyar on 10/31/14.
+//  Copyright (c) 2014 Hellyar. All rights reserved.
+//
+
+import Foundation
+
+
+struct Supplies {
+    
+}
