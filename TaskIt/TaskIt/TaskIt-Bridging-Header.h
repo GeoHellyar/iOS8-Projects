b8713c9acea383ec5fc48ab22e45daf7824cfb5c
mtnbikerbrad <mtnbikerbrad@gmail.com>
2014-11-10 18:16:09 -0800

    addition of Objective C Bridge

---
+++ TaskIt/TaskIt/TaskIt-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  TaskIt-Bridging-Header.h
+//  TaskIt
+//
+//  Created by Brad Hellyar on 11/10/14.
+//  Copyright (c) 2014 SixAxis. All rights reserved.
+//
+
+#import "MGSwipeButton.h"
+#import "MGSwipeTableCell.h"
+
+#ifndef TaskIt_TaskIt_Bridging_Header_h
+#define TaskIt_TaskIt_Bridging_Header_h
+
+
+#endif
